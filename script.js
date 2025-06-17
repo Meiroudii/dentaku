46f08b0398fa511b02948263826463a08b6efab2
Meiroudii <119019700+Meiroudii@users.noreply.github.com>
2025-06-17 11:45:44 +0900

    Added basic calculation logic

--- script.js
+++ script.js
@@ -10,15 +10,13 @@ const expression = [];
 const display = document.getElementById('display');
 const buttons = Array.from(document.querySelectorAll(".calc-button"));
 let num = 0;
+let result = 0;
 
 function toggle_power() {
 	document.body.append(main, power_off);
 	power_off.classList.add("power-off-overlay");
 	alert("Ctrl+R to power on again.");
 }
-
-
-
 
 buttons.forEach((button) => {
 	button.addEventListener("click", (e) => {
@@ -33,11 +31,14 @@ buttons.forEach((button) => {
 
 		} else {
 			display.value = button_label;
+			gui_keys_handler(button_label);
 		}
 	})
 })
 
-window.addEventListener("keyup", (e) => {
+window.addEventListener("keyup", keys_handler);
+
+function keys_handler(e) {
 	const binding = e.key;
 	if (Number.isFinite(parseInt(e.key))) {
 		console.log("That's a number bro!");
@@ -59,70 +60,79 @@ window.addEventListener("keyup", (e) => {
 		display.value = expression
 	}
 	if (expression.length == 3) {
-		let result = 0;
+		result = 0;
 		let x = parseInt(expression[0]);
 		let operator = expression[1];
 		let y = parseInt(expression[2]);
 		display.value = expression
 
-		switch (operator) {
-			case '+':
-				alert(x + y);
-				break;
-			case '-':
-				alert(x - y);
-				result = (x - y);
-				expression.length = 0;
-				expression.push(result);
-				break;
-			case 'x': alert(x * y);
-				break;
-			case '*': alert(x * y);
-				break;
-			case '/':
-				alert(x / y);
-				result = Math.round(x / y);
-				expression.length = 0;
-				expression.push(result);
-				break;
-		}
-
+		operate(x, operator, y);
 	}
 	console.log(e.key);
-})
+}
 
-function operate(expression) {
-	expression = expression.replace(/\s+/g, '');
-	if ('0' <= char && char <= '9') {
-		num += char;
+function gui_keys_handler(expr) {
+	let binding = expr;
+	if (Number.isFinite(parseInt(binding))) {
+		console.log("That's a number bro!");
+		numbers.push(parseInt(binding));
+		num = +numbers.join("");
+		if (expression.length == 2) {
+			expression.push(num);
+			console.log(`At number if ${expression}`);
+		}
+		display.value = expression
 	}
+	if ((binding == "+" || binding == "/" || binding == "*" || binding == "x" || binding == "-")) {
+		if (num != 0) {
+			expression.push(num);
+			expression.push(binding);
+			console.log(expression);
+			numbers.length = 0
+		}
+		display.value = expression
+	}
+	if (expression.length == 3) {
+		result = 0;
+		let x = parseInt(expression[0]);
+		let operator = expression[1];
+		let y = parseInt(expression[2]);
+		display.value = expression
 
+		operate(x, operator, y);
+	}
+	console.log(binding);
 }
 
-
-
-/**
-Thought flow: Should I put the user input into and array? appointing each object on their own operations?
-Putting numbers in variable and another variables for operations?
-Add eventlistener to each buttons that will be designated to their value?
-Add listener similar to key code combo? (keyboard support)
-
-<div data-key="65" class="key">
-	<kbd>A</kbd>
-	<span class="sound">clap</span>
-</div>
-function playSound(e) {
-const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-if (!audio) return;
-
-key.classList.add('playing');
-audio.currentTime = 0;
-audio.play();
+function chain_calculation(result) {
+	expression.length = 0;
+	numbers.length = 0;
+	expression.push(result);
 }
 
-const keys = Array.from(document.querySelectorAll('.key'));
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-window.addEventListener('keydown', playSound);
-**/
-
+function operate(x, operator, y) {
+	switch (operator) {
+		case '+':
+			alert(x + y);
+			chain_calculation(result);
+			break;
+		case '-':
+			alert(x - y);
+			result = (x - y);
+			chain_calculation(result);
+			break;
+		case 'x':
+			alert(x * y);
+			chain_calculation(result);
+			break;
+		case '*':
+			alert(x * y);
+			chain_calculation(result);
+			break;
+		case '/':
+			alert(x / y);
+			result = Math.round(x / y);
+			chain_calculation(result);
+			break;
+	}
+}
